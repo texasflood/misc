4578d564914ad6b280fae7b56d4df8f0c80aa42c
Anas Syed <anas27@gmail.com>
2014-11-05 14:47:10 +0000

    Removed install scripts, moved to configs

--- install/part.sh
+++
@@ -1,9 +0,0 @@
-sudo easy_install psutil #For atp_tex
-read -p "Copy .vim and vimrc files and run :helptags ~/.vim/doc"
-
-#vim terminal
-echo "set editing-mode vi" >> ~/.inputrc
-echo "set keymap vi-command" >> ~/.inputrc
-
-cd ~
-echo "Done"
