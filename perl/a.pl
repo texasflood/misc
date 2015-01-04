511f39e2ac71dc488c913520bb07b743d1422c00
Anas Syed <anas27@gmail.com>
2015-01-04 18:40:39 +0000

    Added perl and ttyecho

---
+++ perl/a.pl
@@ -0,0 +1,6 @@
+#!/usr/bin/perl
+@userinput = <STDIN>;
+foreach $line (@userinput) {
+  print "$line\n";
+  print "Break\n";
+}
---
+++ perl/helloWorld.pl
@@ -0,0 +1,6 @@
+if ("Hello World" =~ /World/) {
+  print "It matches\n";
+}
+else {
+  print "It doesn't match\n";
+}
