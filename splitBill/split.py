d99c0b4fc81ba2ad60a3599b75ea1079fda7d8bd
Anas Syed <anas27@gmail.com>
2015-06-18 19:03:15 +0100

    Finished first draft of split bill

--- splitBill/split.py
+++ splitBill/split.py
@@ -1,44 +1,45 @@
 import sys
 
-people = raw_input('Enter people split by a comma:\n')
-people = [x.strip() for x in people.split(',')]
-if len(people) != len(set(people)):
-  print 'There are duplicates in the list'
-  sys.exit(-1)
+while True:
+  people = raw_input('Enter people split by a comma:\n')
+  people = [x.strip() for x in people.split(',')]
+  if len(people) != len(set(people)):
+    print 'There are duplicates in the list'
+  else:
+    break
 
-print 'People array:'
-print people
+#print 'People array:'
+#print people
 peopleDict = {}
 for person in people:
   idx = 1
   peopleDict[person] = []
   while True:
-    while True:
-      finished = raw_input('Did ' + person + ' pay for anything? (y/n)\n')
-      if finished in ['y', 'n']:
-        break
-      else:
-        print 'Enter either y or n!'
-    if finished == 'n':
+    finished = raw_input('Did ' + person + ' pay for anything? (y/n)\n')
+    if finished in ['y', 'n']:
       break
-    print 'Entering expense number ' + str(idx) + ' for ' + person
-    expense = {}
-    expense['name'] = raw_input('For ' + person + ' enter expense name\n')
-    expense['amount'] = float(raw_input('For expense ' + expense['name'] + ' incurred by ' + person + ', enter amount\n'))
-    expense['responsibles'] = raw_input('Enter people responsible split by a comma:\n')
-    expense['responsibles'] = [x.strip() for x in expense['responsibles'].split(',')]
-    peopleDict[person].append(expense)
-    idx += 1
+    else:
+      print 'Enter either y or n!'
+  if finished != 'n':
     while True:
-      finished = raw_input('Finished with ' + person + '\'s expenses? (y/n)\n')
-      if finished in ['y', 'n']:
+      print 'Entering expense number ' + str(idx) + ' for ' + person
+      expense = {}
+      expense['name'] = raw_input('For ' + person + ' enter expense name\n')
+      expense['amount'] = float(raw_input('For expense ' + expense['name'] + ' incurred by ' + person + ', enter amount\n'))
+      expense['responsibles'] = raw_input('Enter people responsible split by a comma:\n')
+      expense['responsibles'] = [x.strip() for x in expense['responsibles'].split(',')]
+      peopleDict[person].append(expense)
+      idx += 1
+      while True:
+        finished = raw_input('Finished with ' + person + '\'s expenses? (y/n)\n')
+        if finished in ['y', 'n']:
+          break
+        else:
+          print 'Enter either y or n!'
+      if finished == 'y':
         break
-      else:
-        print 'Enter either y or n!'
-    if finished == 'y':
-      break
 
-print peopleDict
+#print peopleDict
 
 owedArray = []
 for i in people:
@@ -51,9 +52,26 @@ for person in people:
         owedArray[people.index(person)][people.index(debtor)] += float(expense['amount'])/len(expense['responsibles'])
         owedArray[people.index(debtor)][people.index(person)] -= float(expense['amount'])/len(expense['responsibles'])
 
+print
 for idx, debtors in enumerate(owedArray):
-  print people[idx] + ' is owed:'
+  printed = False
   for jdx, debtor in enumerate(debtors):
-    print people[jdx] + u' \xA3' + str(debtor)
+    if debtor > 0:
+      if not printed:
+        print people[idx] + ' is owed:'
+        printed = True
 
-print owedArray
+      print '  ' + people[jdx] + u' \xA3%.2f' % debtor
+  print
+
+for idx, debtees in enumerate(owedArray):
+  printed = False
+  for jdx, debtee in enumerate(debtees):
+    if debtee < 0:
+      if not printed:
+        print people[idx] + ' owes:'
+        printed = True
+
+      print '  ' + people[jdx] + u' \xA3%.2f' % -debtee
+
+#print owedArray
