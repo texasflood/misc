b19a861bde7c65b3220a25a51990ad379361f815
Anas Syed <anas.syed@arm.com>
2015-12-24 00:34:23 +0000

    up

---
+++ cTests/tail.cpp
@@ -0,0 +1,55 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <queue>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3) {
+        cout << "Usage: tail [FILE] [NO OF LINES]\n";
+        return -1;
+    }
+
+    int tail_len = std::stoi(argv[2]);
+    std::ifstream file(argv[1]);
+    std::string line;
+    if (file.is_open())
+    {
+        std::queue<std::string> last_lines;
+        int i = 1;
+        bool hit_len = false;
+        while (getline(file, line))
+        {
+            last_lines.push(line);
+            if (hit_len)
+            {
+                last_lines.pop();
+            }
+            else
+            {
+                if (i == tail_len)
+                {
+                    hit_len = true;
+                }
+                else
+                {
+                    ++i;
+                }
+            }
+        }
+        file.close();
+
+        while (!last_lines.empty())
+        {
+            cout << last_lines.front() << endl;
+            last_lines.pop();
+        }
+    }
+    else
+    {
+        cout << "Failed to open file " << argv[1] << "\n";
+    }
+}
